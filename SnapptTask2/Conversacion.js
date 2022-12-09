6a0e6153d8c8a449cd79b5a5efca91f56a9551d5
marcos <marcosdimatteo.wgr@gmail.com>
2022-12-09 14:05:30 -0300

    Clase Usuario y modficación de texto consola

--- SnapptTask2/Conversacion.js
+++ SnapptTask2/Conversacion.js
@@ -1,3 +1,5 @@
+const Usuario = require("./Usuario");
+
 class Conversacion {
 
     #arrayDeMensajes;
@@ -14,58 +16,68 @@ class Conversacion {
     appendMensaje(mensaje) {
 
         this.#arrayDeMensajes.push(mensaje);
+        this.#sort();
+        this.#appendUser(mensaje);
 
     }
 
     // Retorna el último mensaje enviado.
     lastMessage() {
 
-        let retorno = this.#arrayDeMensajes[0];
-        let contenedor;
-        
-        for (let index in this.#arrayDeMensajes) {
-
-            contenedor = this.#arrayDeMensajes[index];
-
-            if (retorno.isOlder(contenedor)) {
-
-                retorno = contenedor;
-
-            }
-
-        }
-
-        return retorno;
+        return this.#arrayDeMensajes[this.#arrayDeMensajes.length - 1];
 
     }
 
     // Retorna el primer mensaje enviado.
-    fistMessage() {
+    firstMessage() {
 
-        let retorno = this.#arrayDeMensajes[0];
-        let contenedor;
-        
-        for (let index in this.#arrayDeMensajes) {
-
-            contenedor = this.#arrayDeMensajes[index];
-
-            if (retorno.isNewer(contenedor)) {
-
-                retorno = contenedor;
-
-            }
-
-        }
-
-        return retorno;
+        return this.#arrayDeMensajes[0];
 
     }
 
     // Retorna la duración de la conversación en milisegundos.
     duration() {
 
-        let tiempo = this.lastMessage().getDate() - this.fistMessage().getDate();
+        let tiempo = this.lastMessage().getDate() - this.firstMessage().getDate();
         return tiempo;
+
+    }
+
+    // Ordena el array de mensajes del más viejo al más nuevo.
+    #sort() {
+
+        this.#arrayDeMensajes.sort(
+            function(x, y) {
+
+                return x.getDate() - y.getDate();
+
+            }
+        );
+
+    }
+
+    #appendUser(mensaje) {
+
+        let flag = false;
+
+        this.#arrayDeUsuarios.forEach(element => {
+            
+            if (element.getNombre() == mensaje.getSender()) {
+
+                flag = true;
+                element.loadMensaje(mensaje);
+
+            }
+
+        });
+
+        if (!flag) {
+
+            let usuario = new Usuario(mensaje.getSender())
+            this.#arrayDeUsuarios.push(usuario);
+            usuario.loadMensaje(mensaje);
+
+        }
 
     }
 
--- SnapptTask2/Main.js
+++ SnapptTask2/Main.js
@@ -12,7 +12,28 @@ for (let msj of mensajes.Mensajes) {
 
 }
 
-console.log("Último mensaje: " + chat.lastMessage().getSender());
-console.log("Duración de la conversación: " + chat.duration());
+console.log("");
 
+console.log("Último mensaje: ");
+console.log("De: " +  chat.lastMessage().getSender());
+console.log("Contenido: " +  chat.lastMessage().getText());
+console.log("Enviado: " + chat.lastMessage().getDate())
+
+console.log("");
+
+console.log("Duración de la conversación: ");
+console.log("En milisegundos -> " + chat.duration() + "ms");
+console.log("En segundos -> "  + (chat.duration() / 1000) + "s");
+console.log("En minutos -> "  + ((chat.duration() / 1000) / 60) + "min");
+
+console.log("");
+
+console.log("Mensajes por usuario: ");
+chat.getArrayDeUsuarios().forEach(element => {
+   
+    console.log(element.getNombre() + ': ' + element.getNumeroDeMensajes());
+
+});
+
+console.log("");
 console.log("Terminó!");
